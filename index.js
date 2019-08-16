c889f4903f12298bdec667a8a5e66edb30c2f6d9
flashfan88 <lavarjohnson20@gmail.com>
2019-08-16 13:47:34 -0500

    imessedup

---
+++ index.js
@@ -0,0 +1,32 @@
+// Problem 1:
+// Put the start of your program in a main function.
+// Create a class for Books with name, rating, genre, and author.
+// Create 3 instances of Books and put them in an array.
+// Afterwards print each one in the format below using a loop:
+// The book [BOOK NAME] has [RATING] stars!!!
+
+function someFunction(elements)
+{
+    class Books
+    {
+        constructor(Name, Rating, Genre, Author)
+        {
+            this.namep = Name;
+            this.ratingp = Rating;
+            this.genrep = Genre;
+            this.authorp = Author;
+
+        }
+    }
+}
+
+
+
+
+let Books = new Books;("Happy Potter", "50 Shades of Grey", "The Hobbit");
+Books.printprops();
+let array1 = ["Harry Potter", "50 Shades of Grey","The Hobbit"];
+
+
+
+
